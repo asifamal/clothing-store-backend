a4d8827ae4a1c5546aedfecbc20533918e309d39
asifamal <asifamal6@gmail.com>
2025-11-17 12:25:25 +0530

    fixed dsign issues nd testing issues

--- cart/test_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('test-auth/', views.TestAuthView.as_view(), name='test_auth'),
-    path('', views.CartView.as_view(), name='cart'),
-    path('add/', views.CartAddView.as_view(), name='cart_add'),
-    path('item/<int:item_id>/', views.CartItemView.as_view(), name='cart_item'),
-]
